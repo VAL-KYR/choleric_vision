9c93ccddf98849e1c09b7afe83fb4aafc011fd3a
VAL-KYR <c418fan@gmail.com>
2017-01-13 22:14:04 -0500

    Scares v0.8
    Added
    - new scare trigger for tape playing
    - new scare trigger for door opening
    - tweak of level scares

--- SeniorProject Unity/Assets/Scripts/triggerRemote.cs
+++ SeniorProject Unity/Assets/Scripts/triggerRemote.cs
@@ -4,10 +4,25 @@ using System.Collections;
 public class triggerRemote : MonoBehaviour {
 
 	public string triggerTag;
+	public GameObject activateObject;
 
 	void OnTriggerEnter()
 	{
 		var go = GameObject.FindWithTag(triggerTag);
-		go.GetComponent<soundTrigger>().arm();
+		if (go.GetComponent<soundTrigger>())
+		{
+			go.GetComponent<soundTrigger>().arm();
+		}
+
+		// activate the activate object
+		if (!activateObject.activeSelf)
+		{
+			activateObject.SetActive(true);
+		}
+		else 
+		{
+			activateObject.SetActive(false);
+		}
+
 	}
 }
