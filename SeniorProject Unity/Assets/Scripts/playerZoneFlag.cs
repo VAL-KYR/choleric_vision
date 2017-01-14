f13f12705d76c3903697dc466d51cae0f10a3d47
VAL-KYR <c418fan@gmail.com>
2017-01-14 15:01:42 -0500

    Scares v0.9
    Added:
    - generate tutorial note script fix
    - flag fixes
    - trigger fixes
    - new note removal from generator task

--- SeniorProject Unity/Assets/Scripts/playerZoneFlag.cs
+++ SeniorProject Unity/Assets/Scripts/playerZoneFlag.cs
@@ -5,15 +5,15 @@ public class playerZoneFlag : MonoBehaviour {
 
     public bool playerFlag = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
 
     void OnTriggerEnter(Collider Entity)
     {
