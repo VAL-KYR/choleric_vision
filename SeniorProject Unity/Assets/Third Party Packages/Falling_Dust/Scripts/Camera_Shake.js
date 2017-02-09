2ca00edd67dee148773d5cde761d0ff046b16834
VAL-KYR <c418fan@gmail.com>
2017-02-08 22:49:59 -0500

    Dust Effects
    Dust effects added

---
+++ SeniorProject Unity/Assets/Third Party Packages/Falling_Dust/Scripts/Camera_Shake.js
@@ -0,0 +1,26 @@
+﻿var CameraShake : GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+
+    if (Input.GetButtonDown("Fire1"))
+    {
+
+        ShakeTheCamera();
+       
+    }
+   
+}
+
+   
+function ShakeTheCamera()
+{
+
+    CameraShake.GetComponent.<Animation>().Play();
+
+}
+
+
