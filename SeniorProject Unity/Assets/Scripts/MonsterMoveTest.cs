ce1f700af654a78a84a06511a064e5704f710ec8
VAL-KYR <c418fan@gmail.com>
2017-02-21 14:07:19 -0500

    Monster Scare Refined
    Added:
    - better movement and look vector lerping
    - textures added to monster

--- SeniorProject Unity/Assets/Scripts/MonsterMoveTest.cs
+++ SeniorProject Unity/Assets/Scripts/MonsterMoveTest.cs
@@ -8,10 +8,13 @@ public class MonsterMoveTest : MonoBehaviour {
     public GameObject[] pos;
     int i = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public Vector3 lastPosition;
+    public Vector3 velocity;
+
+    // Use this for initialization
+    void Start () {
+        lastPosition = new Vector3(0, 0, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,19 +24,27 @@ public class MonsterMoveTest : MonoBehaviour {
             i = 0;
             gameObject.SetActive(false);
         }
-        
 
-        //if (transform.position != pos[i].transform.position && i < 3)
-        //if ((Math.Abs(transform.position.x - pos[i].transform.position.x) <= 0.01f) && (Math.Abs(transform.position.y - pos[i].transform.position.y) <= 0.01f) && (Math.Abs(transform.position.z - pos[i].transform.position.z) <= 0.01f) && i < 3)
-        if(!pos[i].GetComponent<SphereCollider>().bounds.Contains(gameObject.transform.position))
-        {
+        // Hecka good turning code
+        velocity = (transform.position - lastPosition) / Time.deltaTime;
+        lastPosition = transform.position;
+
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation,
+            Quaternion.LookRotation(velocity),
+            Time.deltaTime * 1.0f
+        );
+        //
+
+
+        if (!pos[i].GetComponent<SphereCollider>().bounds.Contains(gameObject.transform.position))
+        {    
             transform.position = Vector3.Lerp(transform.position, pos[i].transform.position, 0.3f * Time.deltaTime);
         }
         else
         {
             i++;
         }
-
-        //transform.position = Vector3.Slerp(transform.position, pos1.transform.position, 0.5f * Time.deltaTime);
     }
+
 }
