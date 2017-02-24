3d5db619293954b17e9a73574c94284d1e22904e
VAL-KYR <c418fan@gmail.com>
2017-02-24 18:02:47 -0500

    Visual Flare & Optimization Update
    Added:
    - desaturation of all colours
    - global fog
    - bloom
    - SSAO
    
    Optimized:
    - Lower level will despawn old assets that are no longer in play with
    UNLOAD_Trigger

---
+++ SeniorProject Unity/Assets/Scripts/fogGlobalSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fogGlobalSettings : MonoBehaviour {
+
+    public Color fogColour;
+
+	// Use this for initialization
+	void Start () {
+        RenderSettings.fogColor = fogColour;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
