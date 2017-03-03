179cf07377c34a458bdec4b0e434431a56cf96e4
VAL-KYR <c418fan@gmail.com>
2017-03-03 11:37:42 -0500

    Small fixes

--- SeniorProject Unity/Assets/Scripts/flashback.cs
+++ SeniorProject Unity/Assets/Scripts/flashback.cs
@@ -4,15 +4,79 @@ using System.Collections;
 public class flashback : MonoBehaviour {
 
     // audio file
+    public GameObject flashbackAudioObject;
+    public GameObject flashbackMusicObject;
+    AudioSource flashbackAudioSource;
+    AudioSource flashbackMusicSource;
+    public AudioClip flashbackAudio;
+    public AudioClip flashbackMusic;
     // visual effects
+    public GameObject vrCam;
+    public GameObject nonVrCam;
+    public bool flashbackTripped = false;
+    public bool flashbackDone = false;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+        vrCam = GameObject.FindGameObjectWithTag("vrCam");
+        nonVrCam = GameObject.FindGameObjectWithTag("NonVRCam");
+
+        if (flashbackAudioObject.GetComponent<AudioSource>() == null)
+        {
+            flashbackAudioObject.AddComponent<AudioSource>();
+            flashbackAudioSource = flashbackAudioObject.GetComponent<AudioSource>();
+        }
+        if (flashbackMusicObject.GetComponent<AudioSource>() == null)
+        {
+            flashbackMusicObject.AddComponent<AudioSource>();
+            flashbackMusicSource = flashbackMusicObject.GetComponent<AudioSource>();
+        }
+
+        flashbackAudioSource.clip = flashbackAudio;
+        flashbackMusicSource.clip = flashbackMusic;
+
+        flashbackDone = false;
+
+        nonVrCam.GetComponent<ColorCorrectionCurves>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if (flashbackAudioSource.isPlaying)
+        {
+            ApplyVisualEffects();
+        }
+
+        else
+        {
+            RemoveVisualEffects();
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("GameController") && !flashbackTripped)
+        {
+            flashbackTripped = true;
+            PlayFlashbackAudio();
+        }
+    }
+
+    public void PlayFlashbackAudio()
+    {
+        flashbackAudioSource.Play();
+        flashbackMusicSource.Play();
+    }
+
+    public void ApplyVisualEffects()
+    {
+
+    }
+
+    public void RemoveVisualEffects()
+    {
+
+    }
 }
--- SeniorProject Unity/Assets/Scripts/monsterSound.cs
+++ SeniorProject Unity/Assets/Scripts/monsterSound.cs
@@ -304,10 +304,11 @@ public class monsterSound : MonoBehaviour {
     {
         if (utterance == "attack")
         {
+            lastMonsterAttackSound = monsterAttackSound;
             RandomizeSounds();
             monsterVoice.clip = monsterAttackSound;
             monsterVoice.PlayDelayed(delay);
-            lastMonsterAttackSound = monsterAttackSound;
+            
         }
         else if (utterance != "attack" && time > randPlayTime && !monsterVoice.isPlaying)
         {
@@ -318,32 +319,37 @@ public class monsterSound : MonoBehaviour {
 
             if (utterance == "alert")
             {
-                monsterVoice.clip = monsterAlertSound;
                 lastMonsterAlertSound = monsterAlertSound;
+                monsterVoice.clip = monsterAlertSound;
+                
             }
                
             if (utterance == "growl")
             {
-                monsterVoice.clip = monsterGrowlSound;
                 lastMonsterGrowlSound = monsterGrowlSound;
+                monsterVoice.clip = monsterGrowlSound;
+               
             }
                
             if (utterance == "notice")
             {
-                monsterVoice.clip = monsterNoticeSound;
                 lastMonsterNoticeSound = monsterNoticeSound;
+                monsterVoice.clip = monsterNoticeSound;
+                
             }
                 
             if (utterance == "ramble")
             {
+                lastMonsterRambleSound = monsterRambleSound;
                 monsterVoice.clip = monsterRambleSound;
-                lastMonsterRambleSound = monsterRambleSound; 
+                
             }   
 
             if (utterance == "search")
             {
-                monsterVoice.clip = monsterSearchSound;
                 lastMonsterRambleSound = monsterRambleSound;
+                monsterVoice.clip = monsterSearchSound;
+                
             }
                
             //}
