6b77e23a9e1c28b27f31ae9ef1fba285b32b332b
VAL-KYR <c418fan@gmail.com>
2017-03-03 16:14:30 -0500

    Flashback Triggers Added

--- SeniorProject Unity/Assets/Scripts/flashback.cs
+++ SeniorProject Unity/Assets/Scripts/flashback.cs
@@ -1,57 +1,76 @@
 ﻿using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.Audio;
 using System.Collections;
 
 public class flashback : MonoBehaviour {
 
-    // audio file
-    public GameObject flashbackAudioObject;
-    public GameObject flashbackMusicObject;
-    AudioSource flashbackAudioSource;
-    AudioSource flashbackMusicSource;
-    public AudioClip flashbackAudio;
-    public AudioClip flashbackMusic;
     // visual effects
     public GameObject vrCam;
     public GameObject nonVrCam;
     public bool flashbackTripped = false;
     public bool flashbackDone = false;
+	public bool voicePlayed;
+
+	public AudioMixerSnapshot flashbackSnapshot;
+	public AudioMixerSnapshot areaMusicSnapshot;
+
+	public float transitionTime = 8.0f;
+	public bool musicPlayed;
+	public float deSatFactor;
+	public float deSatTime = 0.5f;
 
     // Use this for initialization
     void Start () {
-        vrCam = GameObject.FindGameObjectWithTag("vrCam");
-        nonVrCam = GameObject.FindGameObjectWithTag("NonVRCam");
-
-        if (flashbackAudioObject.GetComponent<AudioSource>() == null)
-        {
-            flashbackAudioObject.AddComponent<AudioSource>();
-            flashbackAudioSource = flashbackAudioObject.GetComponent<AudioSource>();
-        }
-        if (flashbackMusicObject.GetComponent<AudioSource>() == null)
-        {
-            flashbackMusicObject.AddComponent<AudioSource>();
-            flashbackMusicSource = flashbackMusicObject.GetComponent<AudioSource>();
-        }
-
-        flashbackAudioSource.clip = flashbackAudio;
-        flashbackMusicSource.clip = flashbackMusic;
-
+		
+        if (!VRSettings.enabled)
+			nonVrCam = GameObject.FindGameObjectWithTag("NonVRCam");
+		else
+			vrCam = GameObject.FindGameObjectWithTag("VRCam");
+		
+		voicePlayed = false;
+		musicPlayed = false;
         flashbackDone = false;
+		deSatFactor = 0.0f;
 
-        nonVrCam.GetComponent<ColorCorrectionCurves>();
+		if (!VRSettings.enabled)
+			nonVrCam.GetComponent<ColorCurvesManager>().Factor = 0.0f;
+		else
+			vrCam.GetComponent<ColorCurvesManager>().Factor = 0.0f;
+		
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (flashbackAudioSource.isPlaying)
-        {
-            ApplyVisualEffects();
-        }
+		if (!VRSettings.enabled)
+		{
+			if(!nonVrCam)
+				nonVrCam = GameObject.FindGameObjectWithTag("NonVRCam");
+			
+			deSatFactor = nonVrCam.GetComponent<ColorCurvesManager>().Factor;
+		}
+		else
+		{
+			if (!vrCam)
+				vrCam = GameObject.FindGameObjectWithTag("VRCam");
+			
+			deSatFactor = vrCam.GetComponent<ColorCurvesManager>().Factor;
+		}
 
-        else
-        {
-            RemoveVisualEffects();
-        }
+
+		if (flashbackTripped && !flashbackDone)
+		{
+			ApplyVisualEffects();
+			PlayFlashbackAudio();
+		}
+
+		else if (flashbackDone && flashbackTripped)
+		{
+			RemoveVisualEffects();
+			areaMusicSnapshot.TransitionTo(transitionTime);
+			Debug.Log("flashback bleed out");
+		}
 
     }
 
@@ -60,23 +79,57 @@ public class flashback : MonoBehaviour {
         if (c.CompareTag("GameController") && !flashbackTripped)
         {
             flashbackTripped = true;
-            PlayFlashbackAudio();
         }
     }
 
     public void PlayFlashbackAudio()
-    {
-        flashbackAudioSource.Play();
-        flashbackMusicSource.Play();
+	{
+		// plays once
+		if (!voicePlayed && deSatFactor > 0.6f) 
+		{
+			gameObject.GetComponent<AudioSource>().Play();
+			voicePlayed = true;
+			Debug.Log("voice played");
+		}
+
+		if (!musicPlayed && !flashbackDone && deSatFactor > 0.1f)
+		{
+			flashbackSnapshot.TransitionTo(transitionTime);
+			musicPlayed = true;
+			Debug.Log("music played");
+		}
+
+		if (voicePlayed && !gameObject.GetComponent<AudioSource>().isPlaying)
+		{
+			flashbackDone = true;
+			Debug.Log("flashback over");
+		}
+        
     }
 
     public void ApplyVisualEffects()
     {
+		if (!VRSettings.enabled)
+		{
+			nonVrCam.GetComponent<ColorCurvesManager>().Factor = Mathf.Lerp(nonVrCam.GetComponent<ColorCurvesManager>().Factor, 1.0f, deSatTime * Time.deltaTime);
+		}
+		else
+		{
+			vrCam.GetComponent<ColorCurvesManager>().Factor = Mathf.Lerp(vrCam.GetComponent<ColorCurvesManager>().Factor, 1.0f, deSatTime * Time.deltaTime);
+		}
+
 
     }
 
     public void RemoveVisualEffects()
     {
-
+		if (!VRSettings.enabled)
+		{
+			nonVrCam.GetComponent<ColorCurvesManager>().Factor = Mathf.Lerp(nonVrCam.GetComponent<ColorCurvesManager>().Factor, 0.0f, deSatTime * Time.deltaTime);
+		}
+		else
+		{
+			vrCam.GetComponent<ColorCurvesManager>().Factor = Mathf.Lerp(vrCam.GetComponent<ColorCurvesManager>().Factor, 0.0f, deSatTime * Time.deltaTime);
+		}
     }
 }
